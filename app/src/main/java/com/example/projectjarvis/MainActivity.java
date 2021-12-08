78d843a6db4b7004b133fbb5220796d20c11e473
johantors <stroeum@gmail.com>
2021-12-08 22:17:23 +0100

    Set up the SSH connection using Ganymed SSH-2 following the lab2 instructions. See the run()-function.
    Also added a simple test to make sure the voiceRecog button actually gives a visual effect (prints "Click!" in the app)

--- app/src/main/java/com/example/projectjarvis/MainActivity.java
+++ app/src/main/java/com/example/projectjarvis/MainActivity.java
@@ -3,14 +3,93 @@ package com.example.projectjarvis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import ch.ethz.ssh2.Connection;
+import ch.ethz.ssh2.Session;
+import ch.ethz.ssh2.StreamGobbler;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton micBtn;
+    TextView voiceInput;
+
+    public void run(String command) { //TODO: Fixa till denna så den är mer "våran"
+        String hostname = "hostname";
+        String username = "username";
+        String password = "password";
+        try {
+            StrictMode.ThreadPolicy policy = new
+                    StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
+            Connection conn = new Connection(hostname); //init connection
+            conn.connect(); //start connection to the hostname
+            boolean isAuthenticated = conn.authenticateWithPassword(username,
+                    password);
+            if (!isAuthenticated)
+                throw new IOException("Authentication failed.");
+            Session session = conn.openSession();
+            session.execCommand(command);
+            InputStream stdout = new StreamGobbler(session.getStdout());
+            BufferedReader br = new BufferedReader(new InputStreamReader(stdout)); //reads text
+
+            while (true) {
+                String line = br.readLine(); // read line
+                if (line == null)
+                    break;
+                System.out.println(line);
+            }
+
+            /* Show exit status, if available (otherwise "null") */
+            System.out.println("ExitCode: " + session.getExitStatus());
+            session.close(); // Close this session
+            conn.close();
+
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
+            System.exit(2);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("TEST");
+        micBtn = findViewById(R.id.voiceBtn);       //button for activating voice recog
+        voiceInput = findViewById(R.id.voiceInput); //textview for showing the voice input
+
+        //TODO: Add a button for activating the actuator for testing
+
+        micBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                voiceInput.setText("Click!");
+                //Start voice recognition
+                //Decode voice input
+                //React to voice input
+            }
+        });
+
+    }
+
+    private void turnOn() {
+        //Turn on actuator
+    }
+
+    private void turnOff() {
+        //Turn off actuator
     }
 }
