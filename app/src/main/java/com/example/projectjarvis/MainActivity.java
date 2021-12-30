d588d4865b98f4a9cd136b46a46b65c45198d688
johantors <stroeum@gmail.com>
2021-12-30 14:52:15 +0100

    Merged MainActivity and Link

--- app/src/main/java/com/example/projectjarvis/MainActivity.java
+++ app/src/main/java/com/example/projectjarvis/MainActivity.java
@@ -7,13 +7,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -24,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
     private TextToSpeech textToSpeech;
     private Controller controller;
     private final Link link = new Link();
+    private static final String FLOOR_LAMP = "project-jarvis/floor-lamp";
+    private MqttAndroidClient client;
+    private static final String SERVER_URI = "tcp://test.mosquitto.org:1883";
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +43,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button turnOnBtn = findViewById(R.id.turnOnBtn);
-        turnOnBtn.setOnClickListener(v -> {
-            link.publish("project-jarvis/floor-lamp", "device/turnOn");
-        });
+        turnOnBtn.setOnClickListener(v -> publish(FLOOR_LAMP, "device/turnOn"));
 
         Button turnOffBtn = findViewById(R.id.turnOffBtn);
         turnOffBtn.setOnClickListener(v -> {
-            link.publish("project-jarvis/floor-lamp", "device/turnOff");
+            publish(FLOOR_LAMP, "device/turnOff");
         });
 
         ImageButton voiceBtn = findViewById(R.id.voiceBtn); //button for activating voice recognition
@@ -53,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         //MQTT Connect
 
         try {
-            link.mqttConnect("project-jarvis/floor-lamp", getApplicationContext()); //TODO variabel
+            mqttConnect(FLOOR_LAMP); //TODO variabel
         } catch (MqttException e) {
             e.printStackTrace();
         }
@@ -99,4 +109,123 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void mqttConnect(String topic) throws MqttException {
+        String feedbackTopic = "project-jarvis/feedback";
+        connect();
+        client.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+                if (reconnect) {
+                    System.out.println("Reconnected to : " + serverURI); // Re-subscribe as we lost it due to new session
+                    subscribe(topic); //TODO - Byt ut
+                    subscribe(feedbackTopic);
+                } else {
+                    System.out.println("Connected to: " + serverURI);
+                    subscribe(topic);
+                    subscribe(feedbackTopic);
+                }
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+                System.out.println("The Connection was lost.");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                String newMessage = new String(message.getPayload());
+                System.out.println("Incoming message: " + newMessage);
+                //txv_light.setText("HEJ");
+
+                /* add code here to interact with elements (text views, buttons)
+                using data from newMessage */
+                //TODO Add code
+                if (topic.equals(feedbackTopic)) {
+                    //feedback-klassen i kontroller?
+                    // feedback(message.decode());
+                }
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+            }
+        });
+    }
+
+    // ** MQTT Connection **
+    // Initialize a client and create a connection to the set broker declared in SERVER_URI.
+    private void connect() {
+        String clientId = MqttClient.generateClientId();
+        client = new MqttAndroidClient(getApplicationContext(), SERVER_URI, clientId);
+        try {
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected Log.d(TAG, “onSuccess”);
+                    System.out.println(TAG + "Success. Connected to " + SERVER_URI);
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Log.d(TAG, "onFailure");
+                    System.out.println(TAG + "Oh no! Failed to connect to " + SERVER_URI);
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void publish(String topic, String message) {
+        try {
+            MqttMessage mqttMessage = new MqttMessage(message.getBytes());
+            mqttMessage.setQos(1);
+            IMqttToken subToken = client.publish(topic, mqttMessage);
+            subToken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    System.out.println("Publish successful to topic: " + topic);
+
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken,
+                                      Throwable exception) {
+                    System.out.println("Failed to publish to topic: " + topic);
+                    // The subscription could not be performed, maybe the user was not
+                    // authorized to subscribe on the specified topic e.g. using wildcards
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Sets a subscription to the set topic, using the client from connect().
+    private void subscribe(String topicToSubscribe) {
+        final String topic = topicToSubscribe;
+        int qos = 1;
+        try {
+            IMqttToken subToken = client.subscribe(topic, qos);
+            subToken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    System.out.println("Subscription successful to topic: " + topic);
+
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken,
+                                      Throwable exception) {
+                    System.out.println("Failed to subscribe to topic: " + topic);
+                    // The subscription could not be performed, maybe the user was not
+                    // authorized to subscribe on the specified topic e.g. using wildcards
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
 }
