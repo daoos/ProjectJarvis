57a1ba7e3e378b613910faac404e2822cda2f431
HugoAhrle <ahrles@live.se>
2022-01-02 13:53:55 +0100

    Vosk update

--- app/src/main/java/com/example/projectjarvis/MainActivity.java
+++ app/src/main/java/com/example/projectjarvis/MainActivity.java
@@ -1,21 +1,28 @@
 package com.example.projectjarvis;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
@@ -25,11 +32,21 @@ import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.vosk.LibVosk;
+import org.vosk.LogLevel;
+import org.vosk.Model;
+import org.vosk.Recognizer;
+import org.vosk.android.RecognitionListener;
+import org.vosk.android.SpeechService;
+import org.vosk.android.SpeechStreamService;
+import org.vosk.android.StorageService;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        RecognitionListener {
 
     private TextView voiceInput;
     private TextToSpeech textToSpeech;
@@ -49,10 +66,42 @@ public class MainActivity extends AppCompatActivity {
     private final Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
     private Ringtone r;
 
+    static private final int STATE_START = 0;
+    static private final int STATE_READY = 1;
+    static private final int STATE_DONE = 2;
+    static private final int STATE_FILE = 3;
+    static private final int STATE_MIC = 4;
+
+    /* Used to handle permission request */
+    private static final int PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
+
+    private Model model;
+    private SpeechService speechService;
+    private SpeechStreamService speechStreamService;
+    private TextView resultView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //------Vosk
+        resultView = findViewById(R.id.result_text);
+        setUiState(STATE_START);
+
+        findViewById(R.id.recognize_mic).setOnClickListener(view -> recognizeMicrophone());
+        ((ToggleButton) findViewById(R.id.pause)).setOnCheckedChangeListener((view, isChecked) -> pause(isChecked));
+
+        LibVosk.setLogLevel(LogLevel.INFO);
+
+        // Check if user has given permission to record audio, init the model after permission is granted
+        int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, PERMISSIONS_REQUEST_RECORD_AUDIO);
+        } else {
+            initModel();
+        }
+        //------Vosk
 
         Button turnOnBtn = findViewById(R.id.turnOnBtn);
         turnOnBtn.setOnClickListener(v -> publish(FLOOR_LAMP, "device/turnOn"));
@@ -303,5 +352,145 @@ public class MainActivity extends AppCompatActivity {
         textToSpeech.speak(string, TextToSpeech.QUEUE_FLUSH, null);
         Toast toast = Toast.makeText(getApplicationContext(), string, Toast.LENGTH_SHORT);
         toast.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSIONS_REQUEST_RECORD_AUDIO) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Recognizer initialization is a time-consuming and it involves IO,
+                // so we execute it in async task
+                initModel();
+            } else {
+                finish();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (speechService != null) {
+            speechService.stop();
+            speechService.shutdown();
+        }
+
+        if (speechStreamService != null) {
+            speechStreamService.stop();
+        }
+    }
+
+    @Override
+    public void onResult(String hypothesis) {
+        resultView.append(hypothesis + "\n");
+    }
+
+    @Override
+    public void onFinalResult(String hypothesis) {
+        resultView.append(hypothesis + "\n");
+        setUiState(STATE_DONE);
+        if (speechStreamService != null) {
+            speechStreamService = null;
+        }
+    }
+
+    @Override
+    public void onPartialResult(String hypothesis) {
+        resultView.append(hypothesis + "\n");
+    }
+
+    @Override
+    public void onError(Exception e) {
+        setErrorState(e.getMessage());
+    }
+
+    @Override
+    public void onTimeout() {
+        setUiState(STATE_DONE);
+    }
+
+    private void initModel() {
+        StorageService.unpack(this, "model-en-us", "model",
+                (model) -> {
+                    this.model = model;
+                    setUiState(STATE_READY);
+                },
+                (exception) -> setErrorState("Failed to unpack the model" + exception.getMessage()));
+    }
+
+    private void recognizeMicrophone() {
+        if (speechService != null) {
+            setUiState(STATE_DONE);
+            speechService.stop();
+            speechService = null;
+        } else {
+            setUiState(STATE_MIC);
+            try {
+                Recognizer rec = new Recognizer(model, 16000.0f);
+                speechService = new SpeechService(rec, 16000.0f);
+                speechService.startListening(this);
+            } catch (IOException e) {
+                setErrorState(e.getMessage());
+            }
+        }
+    }
+
+    private void setErrorState(String message) {
+        resultView.setText(message);
+        ((Button) findViewById(R.id.recognize_mic)).setText(R.string.recognize_microphone);
+        //findViewById(R.id.recognize_file).setEnabled(false);
+        findViewById(R.id.recognize_mic).setEnabled(false);
+    }
+
+    private void setUiState(int state) {
+        switch (state) {
+            case STATE_START:
+                resultView.setText(R.string.preparing);
+                resultView.setMovementMethod(new ScrollingMovementMethod());
+                //findViewById(R.id.recognize_file).setEnabled(false);
+                findViewById(R.id.recognize_mic).setEnabled(false);
+                findViewById(R.id.pause).setEnabled((false));
+                break;
+            case STATE_READY:
+                resultView.setText(R.string.ready);
+                ((Button) findViewById(R.id.recognize_mic)).setText(R.string.recognize_microphone);
+                //findViewById(R.id.recognize_file).setEnabled(true);
+                findViewById(R.id.recognize_mic).setEnabled(true);
+                findViewById(R.id.pause).setEnabled((false));
+                break;
+            case STATE_DONE:
+                //((Button) findViewById(R.id.recognize_file)).setText(R.string.recognize_file);
+                ((Button) findViewById(R.id.recognize_mic)).setText(R.string.recognize_microphone);
+                //findViewById(R.id.recognize_file).setEnabled(true);
+                findViewById(R.id.recognize_mic).setEnabled(true);
+                findViewById(R.id.pause).setEnabled((false));
+                break;
+            case STATE_FILE:
+                //((Button) findViewById(R.id.recognize_file)).setText(R.string.stop_file);
+                resultView.setText(getString(R.string.starting));
+                findViewById(R.id.recognize_mic).setEnabled(false);
+                //findViewById(R.id.recognize_file).setEnabled(true);
+                findViewById(R.id.pause).setEnabled((false));
+                break;
+            case STATE_MIC:
+                ((Button) findViewById(R.id.recognize_mic)).setText(R.string.stop_microphone);
+                resultView.setText(getString(R.string.say_something));
+                //findViewById(R.id.recognize_file).setEnabled(false);
+                findViewById(R.id.recognize_mic).setEnabled(true);
+                findViewById(R.id.pause).setEnabled((true));
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + state);
+        }
+    }
+
+    private void pause(boolean checked) {
+        if (speechService != null) {
+            speechService.setPause(checked);
+        }
     }
 }
