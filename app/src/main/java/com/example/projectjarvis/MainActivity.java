41194995ed799dcf3b03f9ff4874e22762137fa4
johantors <stroeum@gmail.com>
2022-01-03 10:39:09 +0100

    Topic-collection and subscribe to the collection

--- app/src/main/java/com/example/projectjarvis/MainActivity.java
+++ app/src/main/java/com/example/projectjarvis/MainActivity.java
@@ -74,19 +74,19 @@ public class MainActivity extends AppCompatActivity implements
     private static final String TAG = "MainActivity";
 
     //TOPICS
-
-
-    //creates the ringtone / alarm
-    private boolean ringtoneActive = false;
-    private static final Locale SWEDEN = new Locale("sv", "SE");
     private static final String ALARM_TOPIC_CREATE = "project-jarvis/alarm/create";
     private static final String ALARM_TOPIC_CONTROL = "project-jarvis/alarm/control";
     private static final String TIMER_TOPIC_CREATE = "project-jarvis/timer/create";
     private static final String TIMER_TOPIC_CONTROL = "project-jarvis/timer/control";
-    private static final String[] alarmTopics = {ALARM_TOPIC_CONTROL, ALARM_TOPIC_CREATE};
     private static final String SHOPPING_LIST_CREATE = "project-jarvis/shopping-list/create"; //TODO: ONÖDIG?
     private static final String SHOPPING_LIST_CONTROL = "project-jarvis/shopping-list/control";
     private static final String SHOPPING_LIST_READ = "project-jarvis/shopping-list/read";
+    private static final String FEEDBACK_TOPIC = "project-jarvis/feedback";
+    private static final String[] SUBSCRIPTION_TOPICS = {FEEDBACK_TOPIC, ALARM_TOPIC_CONTROL, TIMER_TOPIC_CONTROL};
+
+    //creates the ringtone / alarm
+    private boolean ringtoneActive = false;
+    private static final Locale SWEDEN = new Locale("sv", "SE");
     private final Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
     private Ringtone r;
 
@@ -163,7 +163,7 @@ public class MainActivity extends AppCompatActivity implements
 
         //MQTT Connect
         try {
-            mqttConnect(FLOOR_LAMP);
+            mqttConnect();
         } catch (MqttException e) {
             e.printStackTrace();
         }
@@ -201,8 +201,7 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     // Old LINK
-    public void mqttConnect(String topic) throws MqttException {
-        String feedbackTopic = "project-jarvis/feedback";
+    public void mqttConnect() throws MqttException {
         connect();
         client.setCallback(new MqttCallbackExtended() {
             @Override
@@ -212,12 +211,9 @@ public class MainActivity extends AppCompatActivity implements
                 } else {
                     System.out.println("Connected to: " + serverURI);
                 }
-                subscribe(topic); //TODO - Byt ut?
-                subscribe(feedbackTopic);
-                for (String topic : alarmTopics) {
+                for (String topic : SUBSCRIPTION_TOPICS) {
                     subscribe(topic);
                 }
-                subscribe(TIMER_TOPIC_CONTROL);
             }
 
             @Override
@@ -268,7 +264,7 @@ public class MainActivity extends AppCompatActivity implements
                 } else if (topic.equals(SHOPPING_LIST_READ)) {
                     feedback(newMessage);
                 }
-                if (topic.equals(feedbackTopic)) {
+                if (topic.equals(FEEDBACK_TOPIC)) {
                     System.out.println("FEEDBACK: " + newMessage);
                 }
             }
@@ -594,8 +590,6 @@ public class MainActivity extends AppCompatActivity implements
         String city = cityInput.getText().toString().trim();
         String country = countryInput.getText().toString().trim();
 
-
-
         if (city.equals("")) {
             weatherResult.setText("City field can not be empty!");
         } else {
@@ -752,122 +746,4 @@ public class MainActivity extends AppCompatActivity implements
         }
         return output;
     }
-
-//    BACKUP
-//    private long checkForNumbers(String input) {
-//        boolean isValidInput = true;
-//        long result = 0;
-//        long finalResult = 0;
-//        StringBuilder wordInput = new StringBuilder();
-//
-//        List<String> allowedStrings = Arrays.asList
-//                (
-//                        "zero", "one", "two", "three", "four", "five", "six", "seven",
-//                        "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen",
-//                        "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty",
-//                        "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety",
-//                        "hundred", "thousand", "million", "billion", "trillion"
-//                );
-//
-//        if (input != null && input.length() > 0) {
-//            input = input.replaceAll("-", " ");
-//            input = input.toLowerCase().replaceAll(" and", " ");
-//            String[] splittedParts = input.trim().split("\\s+");
-//
-//            for (String str : splittedParts) {
-//                if (!allowedStrings.contains(str)) {
-//                    isValidInput = false;
-//                    System.out.println("Word found : " + str);
-//                    wordInput.append(str + " ");
-//                } else {
-//
-//                }
-//            }
-//            if (isValidInput) {
-//                System.out.println("VALID INPUT!");
-//                for (String str : splittedParts) {
-//                    if (str.equalsIgnoreCase("zero")) {
-//                        result += 0;
-//                    } else if (str.equalsIgnoreCase("one")) {
-//                        result += 1;
-//                    } else if (str.equalsIgnoreCase("two")) {
-//                        result += 2;
-//                    } else if (str.equalsIgnoreCase("three")) {
-//                        result += 3;
-//                    } else if (str.equalsIgnoreCase("four")) {
-//                        result += 4;
-//                    } else if (str.equalsIgnoreCase("five")) {
-//                        result += 5;
-//                    } else if (str.equalsIgnoreCase("six")) {
-//                        result += 6;
-//                    } else if (str.equalsIgnoreCase("seven")) {
-//                        result += 7;
-//                    } else if (str.equalsIgnoreCase("eight")) {
-//                        result += 8;
-//                    } else if (str.equalsIgnoreCase("nine")) {
-//                        result += 9;
-//                    } else if (str.equalsIgnoreCase("ten")) {
-//                        result += 10;
-//                    } else if (str.equalsIgnoreCase("eleven")) {
-//                        result += 11;
-//                    } else if (str.equalsIgnoreCase("twelve")) {
-//                        result += 12;
-//                    } else if (str.equalsIgnoreCase("thirteen")) {
-//                        result += 13;
-//                    } else if (str.equalsIgnoreCase("fourteen")) {
-//                        result += 14;
-//                    } else if (str.equalsIgnoreCase("fifteen")) {
-//                        result += 15;
-//                    } else if (str.equalsIgnoreCase("sixteen")) {
-//                        result += 16;
-//                    } else if (str.equalsIgnoreCase("seventeen")) {
-//                        result += 17;
-//                    } else if (str.equalsIgnoreCase("eighteen")) {
-//                        result += 18;
-//                    } else if (str.equalsIgnoreCase("nineteen")) {
-//                        result += 19;
-//                    } else if (str.equalsIgnoreCase("twenty")) {
-//                        result += 20;
-//                    } else if (str.equalsIgnoreCase("thirty")) {
-//                        result += 30;
-//                    } else if (str.equalsIgnoreCase("forty")) {
-//                        result += 40;
-//                    } else if (str.equalsIgnoreCase("fifty")) {
-//                        result += 50;
-//                    } else if (str.equalsIgnoreCase("sixty")) {
-//                        result += 60;
-//                    } else if (str.equalsIgnoreCase("seventy")) {
-//                        result += 70;
-//                    } else if (str.equalsIgnoreCase("eighty")) {
-//                        result += 80;
-//                    } else if (str.equalsIgnoreCase("ninety")) {
-//                        result += 90;
-//                    } else if (str.equalsIgnoreCase("hundred")) {
-//                        result *= 100;
-//                    } else if (str.equalsIgnoreCase("thousand")) {
-//                        result *= 1000;
-//                        finalResult += result;
-//                        result = 0;
-//                    } else if (str.equalsIgnoreCase("million")) {
-//                        result *= 1000000;
-//                        finalResult += result;
-//                        result = 0;
-//                    } else if (str.equalsIgnoreCase("billion")) {
-//                        result *= 1000000000;
-//                        finalResult += result;
-//                        result = 0;
-//                    } else if (str.equalsIgnoreCase("trillion")) {
-//                        result *= 1000000000000L;
-//                        finalResult += result;
-//                        result = 0;
-//                    }
-//                }
-//
-//                finalResult += result;
-//                result = 0;
-//                System.out.println("FINAL RESULT: " + finalResult);
-//            }
-//        }
-//        return finalResult;
-//    }
 }
